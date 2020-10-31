630b7c9e45f84804bad4f775f95f9f668fdc143d
ktprograms <keith.tjg@gmail.com>
2020-10-31 10:19:28 +0800

    Remove Tests

--- app/src/main/java/com/ktprograms/watertracker/AlarmReceiver.kt
+++ app/src/main/java/com/ktprograms/watertracker/AlarmReceiver.kt
@@ -26,6 +26,7 @@ import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 
